04aaef2a00475ef17a7fb301ba455408f880035a
RodrigoPG-W <61446157+RodrigoPG-W@users.noreply.github.com>
2020-04-15 01:33:07 -0500

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,13 @@
+#Se pide el nombre al usuario
+print ("Hola, de lejitos por el COVID, ¨como te llamas?")
+#Se leen los datos introducidos por el usuario y se asignan a la variable nombre
+nombre = input()
+#Se escribe el nombre solicitado
+print ("Buen dia {}".format(nombre)
+print ("---Calculadora---")          #Opciones para el usuario 
+print ("1- Sumar")
+print ("2- Restar")
+print ("3- Multiplicar")
+print ("4- Dividir")
+print ("5- Salir")
+op = int(input('Opcion: '))
